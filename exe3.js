40edeb3a67ba929ac67003296e7f7817dacdca2a
killerdias <143296784+killerdias@users.noreply.github.com>
2023-10-19 17:28:11 -0300

    Add files via upload

---
+++ exe3.js
@@ -0,0 +1,16 @@
+alert ('Bem vindo a lista 03')
+alert ('Vai contar até 10 ordem crescente')
+let numero = 1;
+while (numero <= 10) {
+    console.log(numero);
+    numero++;
+    alert(numero)
+ 
+}
+alert ('Vai contar até 10 ordem descrecente')
+let numero1 = 10;
+while (numero1 >= 0){
+    console.log(numero1);
+    numero1--;
+    alert(numero1)
+}
